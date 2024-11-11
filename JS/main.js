3a15cfbb9df1c977645b73ae4032b5b333005431
MuStaFa KhaShaBa <moustafa_165736@fci.sohag.edu.eg>
2024-11-11 20:54:01 +0200

    add images seo and title

--- JS/main.js
+++ JS/main.js
@@ -7,12 +7,12 @@ function setLanguage(lang) {
     if (lang === 'en') {
         elementsEn.forEach(el => el.classList.remove('d-none'));
         elementsAr.forEach(el => el.classList.add('d-none'));
-        document.title = "The cheapest painting and professional finishing in Kuwait - Al Hassan for Finishing."
+        document.title = "The cheapest painting and professional finishing in Kuwait - Al Hussien for Finishing."
 
     } else if (lang === 'ar') {
         elementsEn.forEach(el => el.classList.add('d-none'));
         elementsAr.forEach(el => el.classList.remove('d-none'));
-        document.title = "أرخص صباغة وتشطيب احترافي بالكويت - ال حسن للتشطيبات"
+        document.title = "صباغ - صباغ رخيص - صباغ شاطر ورخيص - بالكويت 0096596930778 - صباغ رخيص بالكويت - رقم صباغ رخيص - صباغ ورق جدران - صباغ الكويت رخيص - صباغ هندي - الحسين للتشطيبات"
     }
 }
 
@@ -43,3 +43,31 @@ document.querySelectorAll("#portfolio .tab-content .port")
     .forEach((i) => i.addEventListener("click", _ =>
         i.classList.contains("active") ? i.classList.remove("active") :
             i.classList.add("active")))
+
+
+
+/// hide images 
+// Get all images in the container
+const images = document.querySelectorAll('.img-profile img');
+let currentIndex = 0;
+
+// Function to show the current image and hide others with fade effect
+function showImage(index) {
+  images.forEach((img, i) => {
+    if (i === index) {
+      img.classList.add('show'); // Add show class to fade in
+    } else {
+      img.classList.remove('show'); // Remove show class to fade out
+    }
+  });
+}
+
+// Initial display of the first image
+showImage(currentIndex);
+
+// Set interval to change the displayed image every 3 seconds
+setInterval(() => {
+  // Update the index to show the next image
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}, 5000); // Change 3000 to your desired interval in milliseconds
